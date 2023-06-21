2075be0e742600ac15e443d407457d60a2956cec
Zakhar Kibanov <z.kibanov@yandex.ru>
2023-06-21 18:28:00 +0300

    DZ3 - покрыл тестами, рефакторинг кода по результатам КР

--- tree.js
+++ tree.js
@@ -1,4 +1,3 @@
-const [, , path, opts, _depth] = process.argv;
 const fs = require('fs');
 
 function drawPath(pathName, depth, isLast) {
@@ -6,29 +5,39 @@ function drawPath(pathName, depth, isLast) {
     return '| '.repeat(depth - 1) + currentPath;
 }
 
-let initialDepth = 1;
-if (opts === '-d' && !isNaN(Number(_depth))) {
-    initialDepth = Number(_depth);
-}
-let fileCount = 0;
-let dirCount = 0;
-let dirInfo = '';
-
-const recursiveGetFs = (name, depth) => {
+const recursiveGetFs = (
+    name = './', 
+    depth, 
+    initialDepth = 1, 
+    dirInfo = '',
+    dirCount = 0,
+    fileCount = 0
+    ) => {
     const currentFiles = fs.readdirSync(name, { withFileTypes: true })
     currentFiles.forEach((el, idx) => {
         dirInfo += (drawPath(el.name, depth, idx === currentFiles.length - 1))
         if (el.isDirectory()) {
             dirCount += 1;
             if (depth < initialDepth) {
-                recursiveGetFs(`${name}/${el.name}`, depth + 1)
+                const [_dirInfo, _dirCount, _fileCount] = recursiveGetFs(`${name}/${el.name}`, depth + 1, initialDepth);
+                dirInfo += _dirInfo;
+                dirCount += _dirCount;
+                fileCount += _fileCount;
             }
         } else {
             fileCount += 1;
         }
     })
+    return [dirInfo, dirCount, fileCount]
 }
 
-recursiveGetFs(path, 1);
-console.log(dirInfo)
-console.log(`${dirCount} directories ${fileCount} files`)
+function tree(path, opts, _depth) {
+    let initialDepth = 1;
+    if (opts === '-d' && !isNaN(Number(_depth))) {
+        initialDepth = Number(_depth);
+    }
+    const [dirInfo, dirCount, fileCount] = recursiveGetFs(path, 1, initialDepth);
+    return (`${dirInfo}\n${dirCount} directories ${fileCount} files`)
+}
+
+module.exports = { tree, recursiveGetFs, drawPath }
